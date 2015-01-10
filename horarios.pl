215ab9f0398597e65b6a2291567122ba017e1638
Francisco Santos <1111315@isep.ipp.pt>
2015-01-10 14:07:26 +0000

    Segmentação de código. Base de conhecimento(horarios, linhas, pontosInteresse) separada da lógica (pesquisas)

---
+++ horarios.pl
@@ -0,0 +1,159 @@
+%% 2. Horarios
+%%DESCRICAO
+%%horario(n_linha,estacao_destino/sentido,hora_abertura,minuto_abertura,hora_fecho,minuto_fecho,dia<0 para domingo,segunda,terca,quarta,quinta ou 1 para sexta e sabado>).
+
+%% ## LINHA 1 ##
+%%linha 1 com sentido a 'La Defense Grande Arche' com inicio as 5h30 e fim as 0h40 em dias da semana e domingo.
+horario(1,'La Defense Grande Arche',5,30,0,40,0).
+%%linha 1 com sentido a 'La Defense Grande Arche' com inicio as 5h30 e fim as 1h40 em dias de sexta e sabado.
+horario(1,'La Defense Grande Arche',5,30,1,40,1).
+
+%%linha 1 com sentido a 'Chateau de Vincennes' com inicio as 5h30 e fim as 0h40 em dias da semana e domingo.
+horario(1,'Chateau de Vincennes',5,30,0,40,0).
+%%linha 1 com sentido a 'Chateau de Vincennes' com inicio as 5h30 e fim as 1h40 em dias de sexta e sabado.
+horario(1,'Chateau de Vincennes',5,30,1,40,1).
+
+%% ## LINHA 2 ##
+%%linha 2 com sentido a 'Porte Dauphine – Marechal de Lattre de Tassigny' com inicio as 5h30 e fim as 0h40 em dias da semana e domingo.
+horario(2,'Porte Dauphine – Marechal de Lattre de Tassigny',5,30,0,40,0).
+%%linha 2 com sentido a 'Porte Dauphine – Marechal de Lattre de Tassigny' com inicio as 5h30 e fim as 1h40 em dias de sexta e sabado.
+horario(2,'Porte Dauphine – Marechal de Lattre de Tassigny',5,30,1,40,1).
+
+%%linha 2 com sentido a 'Nation' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(2,'Nation',5,30,0,42,0).
+%%linha 2 com sentido a 'Nation' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(2,'Nation',5,30,1,42,1).
+
+%% ## LINHA 3 ##
+%%linha 3 com sentido a 'Pont de Levallois — Becon' com inicio as 5h30 e fim as 0h45 em dias da semana e domingo.
+horario(3,'Pont de Levallois — Becon',5,30,0,45,0).
+%%linha 3 com sentido a 'Pont de Levallois — Becon' com inicio as 5h30 e fim as 1h45 em dias de sexta e sabado.
+horario(3,'Pont de Levallois — Becon',5,30,1,45,1).
+
+%%linha 3 com sentido a 'Gallieni' com inicio as 5h30 e fim as 0h44 em dias da semana e domingo.
+horario(3,'Gallieni',5,30,0,44,0).
+%%linha 3 com sentido a 'Gallieni' com inicio as 5h30 e fim as 1h44 em dias de sexta e sabado.
+horario(3,'Gallieni',5,30,1,44,1).
+
+%% ## LINHA 4 ##
+%%linha 4 com sentido a 'Porte de Clignancourt' com inicio as 5h30 e fim as 0h47 em dias da semana e domingo.
+horario(4,'Porte de Clignancourt',5,30,0,47,0).
+%%linha 4 com sentido a 'Porte de Clignancourt' com inicio as 5h30 e fim as 1h47 em dias de sexta e sabado.
+horario(4,'Porte de Clignancourt',5,30,1,47,1).
+
+%%linha 4 com sentido a 'Porte dOrleans – General Leclerc' com inicio as 5h30 e fim as 0h47 em dias da semana e domingo.
+horario(4,'Porte dOrleans – General Leclerc',5,30,0,47,0).
+%%linha 4 com sentido a 'Porte dOrleans – General Leclerc' com inicio as 5h30 e fim as 1h47 em dias de sexta e sabado.
+horario(4,'Porte dOrleans – General Leclerc',5,30,1,47,1).
+
+%% ## LINHA 5 ##
+%%linha 5 com sentido a 'Bobigny — Pablo Picasso Prefecture — Hôtel du Departement' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(5,'Bobigny — Pablo Picasso Prefecture — Hôtel du Departement',5,30,0,42,0).
+%%linha 5 com sentido a 'Bobigny — Pablo Picasso Prefecture — Hôtel du Departement' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(5,'Bobigny — Pablo Picasso Prefecture — Hôtel du Departement',5,30,1,42,1).
+
+%%linha 5 com sentido a 'Place dItalie' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
+horario(5,'Place dItalie',5,30,0,41,0).
+%%linha 5 com sentido a 'Place dItalie' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
+horario(5,'Place dItalie',5,30,1,41,1).
+
+################################ PAREI AQUI. DAQUI PARA A FRENTE É PRECISO POR OS HORARIOS ###############################
+
+%% ## LINHA 6 ##
+%%linha 6 com sentido a 'Charles de Gaulle — Etoile' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(6,'Charles de Gaulle — Etoile',5,30,0,42,0).
+%%linha 6 com sentido a 'Charles de Gaulle — Etoile' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(6,'Charles de Gaulle — Etoile',5,30,1,42,1).
+
+%%linha 6 com sentido a 'Nation' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
+horario(6,'Nation',5,30,0,41,0).
+%%linha 6 com sentido a 'Nation' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
+horario(6,'Nation',5,30,1,41,1).
+
+%% ## LINHA 7 ##
+%%linha 7 com sentido a 'La Courneuve — 8 Mai 1945' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(7,'La Courneuve — 8 Mai 1945',5,30,0,42,0).
+%%linha 7 com sentido a 'La Courneuve — 8 Mai 1945' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(7,'La Courneuve — 8 Mai 1945',5,30,1,42,1).
+
+%%linha 7 com sentido a 'Mairie dIvry' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
+horario(7,'Mairie dIvry',5,30,0,41,0).
+%%linha 7 com sentido a 'Mairie dIvry' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
+horario(7,'Mairie dIvry',5,30,1,41,1).
+
+%% ## LINHA 8 ##
+%%linha 8 com sentido a 'Balard' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(8,'Balard',5,30,0,42,0).
+%%linha 8 com sentido a 'Balard' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(8,'Balard',5,30,1,42,1).
+
+%%linha 8 com sentido a 'Creteil — Prefecture Hotel de Ville' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
+horario(8,'Creteil — Prefecture Hotel de Ville',5,30,0,41,0).
+%%linha 8 com sentido a 'Creteil — Prefecture Hotel de Ville' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
+horario(8,'Creteil — Prefecture Hotel de Ville',5,30,1,41,1).
+
+%% ## LINHA 9 ##
+%%linha 9 com sentido a 'Pont de Sevres' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(9,'Pont de Sevres',5,30,0,42,0).
+%%linha 9 com sentido a 'Pont de Sevres' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(9,'Pont de Sevres',5,30,1,42,1).
+
+%%linha 9 com sentido a 'Mairie de Montreuil' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
+horario(9,'Mairie de Montreuil',5,30,0,41,0).
+%%linha 9 com sentido a 'Mairie de Montreuil' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
+horario(9,'Mairie de Montreuil',5,30,1,41,1).
+
+%% ## LINHA 10 ##
+%%linha 10 com sentido a 'Boulogne — Pont de Saint-Cloud Rhin et Danube' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(10,'Boulogne — Pont de Saint-Cloud Rhin et Danube',5,30,0,42,0).
+%%linha 10 com sentido a 'Boulogne — Pont de Saint-Cloud Rhin et Danube' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(10,'Boulogne — Pont de Saint-Cloud Rhin et Danube',5,30,1,42,1).
+
+%%linha 10 com sentido a 'Gare dAusterlitz' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
+horario(10,'Gare dAusterlitz',5,30,0,41,0).
+%%linha 10 com sentido a 'Gare dAusterlitz' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
+horario(10,'Gare dAusterlitz',5,30,1,41,1).
+
+%% ## LINHA 11 ##
+%%linha 11 com sentido a 'Chatelet' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(11,'Chatelet',5,30,0,42,0).
+%%linha 11 com sentido a 'Chatelet' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(11,'Chatelet',5,30,1,42,1).
+
+%%linha 11 com sentido a 'Mairie des Lilas' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
+horario(11,'Mairie des Lilas',5,30,0,41,0).
+%%linha 11 com sentido a 'Mairie des Lilas' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
+horario(11,'Mairie des Lilas',5,30,1,41,1).
+
+%% ## LINHA 12 ##
+%%linha 12 com sentido a 'Porte de la Chapelle' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(12,'Porte de la Chapelle',5,30,0,42,0).
+%%linha 12 com sentido a 'Porte de la Chapelle' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(12,'Porte de la Chapelle',5,30,1,42,1).
+
+%%linha 12 com sentido a 'Mairie dIssy' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
+horario(12,'Mairie dIssy',5,30,0,41,0).
+%%linha 12 com sentido a 'Mairie dIssy' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
+horario(12,'Mairie dIssy',5,30,1,41,1).
+
+%% ## LINHA 13 ##
+%%linha 13 com sentido a 'Asnieres — Gennevilliers Les Courtilles' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(13,'Asnieres — Gennevilliers Les Courtilles',5,30,0,42,0).
+%%linha 13 com sentido a 'Asnieres — Gennevilliers Les Courtilles' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(13,'Asnieres — Gennevilliers Les Courtilles',5,30,1,42,1).
+
+%%linha 13 com sentido a 'Chatillon — Montrouge' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
+horario(13,'Chatillon — Montrouge',5,30,0,41,0).
+%%linha 13 com sentido a 'Chatillon — Montrouge' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
+horario(13,'Chatillon — Montrouge',5,30,1,41,1).
+
+%% ## LINHA 14 ##
+%%linha 14 com sentido a 'Saint-Lazare' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
+horario(14,'Saint-Lazare',5,30,0,42,0).
+%%linha 14 com sentido a 'Saint-Lazare' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
+horario(14,'Saint-Lazare',5,30,1,42,1).
+
+%%linha 14 com sentido a 'Olympiades' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
+horario(14,'Olympiades',5,30,0,41,0).
+%%linha 14 com sentido a 'Olympiades' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
+horario(14,'Olympiades',5,30,1,41,1).
