424c402c8c9c8a7f9997852f28e761c3bcc57c61
Francisco Santos <1111315@isep.ipp.pt>
2015-01-10 14:05:44 +0000

    Adicionados alguns pontos de interesse.

--- main.pl
+++ main.pl
@@ -210,9 +210,81 @@ horario(14,'Olympiades',5,30,1,41,1).
 horario().
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% 4. Pontos de interesse
-pontoInteresse().
-pontoInteresse().
+% 4. Pontos de interesse
+/*ponto de interesse pdi(estacaoEmQueSeEncontra, nomeDoPontoDeInteresse).*/
+%linha 1
+pdi('Puteax','La Defense Grande Arche').
+pdi('Grande Arche','La Defense Grande Arche').
+pdi('Esplanade de la Défense', 'Courbevoie').
+pdi('Esplanade de la Défense', 'Puteax').
+pdi('Pont de Neuilly','Neuilly-sur-Seine').
+pdi('Les Sablons Jardin d’Acclimatation','Neuilly-sur-Seine').
+pdi('Saint-Mandé','Saint-Mandé').
+pdi('Bérault','Saint-Mandé').
+pdi('Bérault','Vincennes').
+pdi('Château de Vincennes','Vincennes').
+%linha 3
+pdi('Pont de Levallois — Bécon','Levallois-Perret').
+pdi('Anatole France	','Levallois-Perret').
+pdi('Louise Michel','Levallois-Perret').
+pdi('Porte de Bagnolet','Bagnolet').
+pdi('Gallieni','Bagnolet').
+%linha 5
+pdi('Bobigny — Pablo Picasso Préfecture — Hôtel du Département','Bobigny').
+pdi('Bobigny — Pantin — Raymond Queneau	','Bobigny').
+pdi('Bobigny — Pantin — Raymond Queneau','Pantin').
+pdi('Église de Pantin','Pantin').
+pdi('Hoche','Pantin').
+%linha 7
+pdi('La Courneuve — 8 Mai 1945','La Courneuve').
+pdi('Fort d’Aubervilliers','Aubervilliers').
+pdi('Aubervilliers — Pantin — Quatre Chemins','Aubervilliers').
+pdi('Aubervilliers — Pantin — Quatre Chemins','Pantin').
+pdi('Tolbiac','Le Kremlin-Bicêtre').
+pdi('Maison Blanche','Villejuif').
+pdi('Le Kremlin-Bicêtre','Villejuif').
+pdi('Villejuif — Léo Lagrange','Villejuif').
+pdi('Porte de Choisy','Ivry-sur-Seine').
+pdi('Porte d’Ivry','Ivry-sur-Seine').
+pdi('Pierre et Marie Curie','Ivry-sur-Seine').
+pdi('Mairie d’Ivry','Ivry-sur-Seine').
+%linha 8
+pdi('Liberté','Charenton-le-Pont').
+pdi('Charenton — Écoles – Place Aristide Briand','Charenton-le-Pont').
+pdi('École Vétérinaire de Maisons-Alfort','Maisons-Alfort').
+pdi('Maisons-Alfort — Stade','Maisons-Alfort').
+pdi('Maisons-Alfort — Les Juilliottes','Maisons-Alfort').
+pdi('Créteil — L’Échat Hôpital Henri Mondor','Créteil').
+pdi('Créteil — Université','Créteil').
+pdi('Créteil — Préfecture Hôtel de Ville','Créteil').
+%linha 9
+pdi('Pont de Sèvres','Boulogne-Billancourt').
+pdi('Billancourt','Boulogne-Billancourt').
+pdi('Marcel Sembat','Boulogne-Billancourt').
+pdi('Robespierre','Montreuil').
+pdi('Croix de Chavaux – Place Jacques Duclos','Montreuil').
+pdi('Mairie de Montreuil','Montreuil').
+%linha 10
+pdi('Boulogne — Pont de Saint-Cloud Rhin et Danube','Boulogne-Billancourt').
+pdi('Boulogne — Jean Jaurès','Boulogne-Billancourt').
+%linha 11
+pdi('Mairie des Lilas','').
+%linha 12
+pdi('Corentin Celton','Issy-les-Moulineaux').
+pdi('Mairie d’Issy','Issy-les-Moulineaux').
+%linha 13
+pdi('Asnières — Gennevilliers Les Courtilles','Asnières-sur-Seine, Gennevilliers').
+pdi('Les Agnettes','Asnières-sur-Seine, Gennevilliers').
+pdi('Gabriel Péri','Asnières-sur-Seine, Gennevilliers').
+pdi('Mairie de Clichy','Clichy').
+pdi('Saint-Denis','Université Basilique de Saint-Denis').
+pdi('Saint-Denis','Saint-Denis').
+pdi('Hôtel de Ville','Saint-Denis').
+pdi('Malakoff — Plateau de Vanves','Malakoff').
+pdi('Malakoff — Rue Etienne Dolet','Malakoff').
+pdi('Malakoff — Plateau de Vanves','Vanves').
+pdi('Châtillon — Montrouge','Châtillon').
+pdi('Châtillon — Montrouge','Montrouge').
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%% Algoritmos auxiliares %%%%%%%%
