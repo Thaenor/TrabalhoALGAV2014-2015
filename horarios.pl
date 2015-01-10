5c8ae2bbfb8ebab939e5ee0ac38a036ba7218ae5
Rui Silva <1121296@isep.ipp.pt>
2015-01-10 17:10:18 +0000

    Correções nas linhas e adicionado o resto dos horários

--- horarios.pl
+++ horarios.pl
@@ -57,8 +57,6 @@ horario(5,'Place dItalie',5,30,0,41,0).
 %%linha 5 com sentido a 'Place dItalie' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
 horario(5,'Place dItalie',5,30,1,41,1).
 
-################################ PAREI AQUI. DAQUI PARA A FRENTE É PRECISO POR OS HORARIOS ###############################
-
 %% ## LINHA 6 ##
 %%linha 6 com sentido a 'Charles de Gaulle — Etoile' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
 horario(6,'Charles de Gaulle — Etoile',5,30,0,42,0).
@@ -70,90 +68,92 @@ horario(6,'Nation',5,30,0,41,0).
 %%linha 6 com sentido a 'Nation' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
 horario(6,'Nation',5,30,1,41,1).
 
-%% ## LINHA 7 ##
-%%linha 7 com sentido a 'La Courneuve — 8 Mai 1945' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
-horario(7,'La Courneuve — 8 Mai 1945',5,30,0,42,0).
-%%linha 7 com sentido a 'La Courneuve — 8 Mai 1945' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
-horario(7,'La Courneuve — 8 Mai 1945',5,30,1,42,1).
+################################ PAREI AQUI. DAQUI PARA A FRENTE É PRECISO POR OS HORARIOS ###############################
 
-%%linha 7 com sentido a 'Mairie dIvry' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
-horario(7,'Mairie dIvry',5,30,0,41,0).
-%%linha 7 com sentido a 'Mairie dIvry' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
-horario(7,'Mairie dIvry',5,30,1,41,1).
+%% ## LINHA 7 ##
+%%linha 7 com sentido a 'La Courneuve — 8 Mai 1945' com inicio as 5h30 e fim as 0h27 em dias da semana e domingo.
+horario(7,'La Courneuve — 8 Mai 1945',5,30,0,27,0).
+%%linha 7 com sentido a 'La Courneuve — 8 Mai 1945' com inicio as 5h30 e fim as 1h27 em dias de sexta e sabado.
+horario(7,'La Courneuve — 8 Mai 1945',5,30,1,27,1).
+
+%%linha 7 com sentido a 'Mairie dIvry' com inicio as 5h30 e fim as 0h20 em dias da semana e domingo.
+horario(7,'Mairie dIvry',5,30,0,20,0).
+%%linha 7 com sentido a 'Mairie dIvry' com inicio as 5h30 e fim as 1h20 em dias de sexta e sabado.
+horario(7,'Mairie dIvry',5,30,1,20,1).
 
 %% ## LINHA 8 ##
-%%linha 8 com sentido a 'Balard' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
-horario(8,'Balard',5,30,0,42,0).
-%%linha 8 com sentido a 'Balard' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
-horario(8,'Balard',5,30,1,42,1).
+%%linha 8 com sentido a 'Balard' com inicio as 5h30 e fim as 0h24 em dias da semana e domingo.
+horario(8,'Balard',5,30,0,24,0).
+%%linha 8 com sentido a 'Balard' com inicio as 5h30 e fim as 1h24 em dias de sexta e sabado.
+horario(8,'Balard',5,30,1,24,1).
 
-%%linha 8 com sentido a 'Creteil — Prefecture Hotel de Ville' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
-horario(8,'Creteil — Prefecture Hotel de Ville',5,30,0,41,0).
-%%linha 8 com sentido a 'Creteil — Prefecture Hotel de Ville' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
-horario(8,'Creteil — Prefecture Hotel de Ville',5,30,1,41,1).
+%%linha 8 com sentido a 'Creteil — Prefecture Hotel de Ville' com inicio as 5h30 e fim as 0h24 em dias da semana e domingo.
+horario(8,'Creteil — Prefecture Hotel de Ville',5,30,0,24,0).
+%%linha 8 com sentido a 'Creteil — Prefecture Hotel de Ville' com inicio as 5h30 e fim as 1h24 em dias de sexta e sabado.
+horario(8,'Creteil — Prefecture Hotel de Ville',5,30,1,24,1).
 
 %% ## LINHA 9 ##
-%%linha 9 com sentido a 'Pont de Sevres' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
-horario(9,'Pont de Sevres',5,30,0,42,0).
-%%linha 9 com sentido a 'Pont de Sevres' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
-horario(9,'Pont de Sevres',5,30,1,42,1).
+%%linha 9 com sentido a 'Pont de Sevres' com inicio as 5h30 e fim as 0h26 em dias da semana e domingo.
+horario(9,'Pont de Sevres',5,30,0,26,0).
+%%linha 9 com sentido a 'Pont de Sevres' com inicio as 5h30 e fim as 1h26 em dias de sexta e sabado.
+horario(9,'Pont de Sevres',5,30,1,26,1).
 
-%%linha 9 com sentido a 'Mairie de Montreuil' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
-horario(9,'Mairie de Montreuil',5,30,0,41,0).
-%%linha 9 com sentido a 'Mairie de Montreuil' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
-horario(9,'Mairie de Montreuil',5,30,1,41,1).
+%%linha 9 com sentido a 'Mairie de Montreuil' com inicio as 5h30 e fim as 0h26 em dias da semana e domingo.
+horario(9,'Mairie de Montreuil',5,30,0,26,0).
+%%linha 9 com sentido a 'Mairie de Montreuil' com inicio as 5h30 e fim as 1h26 em dias de sexta e sabado.
+horario(9,'Mairie de Montreuil',5,30,1,26,1).
 
 %% ## LINHA 10 ##
-%%linha 10 com sentido a 'Boulogne — Pont de Saint-Cloud Rhin et Danube' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
-horario(10,'Boulogne — Pont de Saint-Cloud Rhin et Danube',5,30,0,42,0).
-%%linha 10 com sentido a 'Boulogne — Pont de Saint-Cloud Rhin et Danube' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
-horario(10,'Boulogne — Pont de Saint-Cloud Rhin et Danube',5,30,1,42,1).
+%%linha 10 com sentido a 'Boulogne — Pont de Saint-Cloud Rhin et Danube' com inicio as 5h30 e fim as 0h21 em dias da semana e domingo.
+horario(10,'Boulogne — Pont de Saint-Cloud Rhin et Danube',5,30,0,21,0).
+%%linha 10 com sentido a 'Boulogne — Pont de Saint-Cloud Rhin et Danube' com inicio as 5h30 e fim as 1h21 em dias de sexta e sabado.
+horario(10,'Boulogne — Pont de Saint-Cloud Rhin et Danube',5,30,1,21,1).
 
-%%linha 10 com sentido a 'Gare dAusterlitz' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
-horario(10,'Gare dAusterlitz',5,30,0,41,0).
-%%linha 10 com sentido a 'Gare dAusterlitz' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
-horario(10,'Gare dAusterlitz',5,30,1,41,1).
+%%linha 10 com sentido a 'Gare dAusterlitz' com inicio as 5h30 e fim as 0h47 em dias da semana e domingo.
+horario(10,'Gare dAusterlitz',5,30,0,47,0).
+%%linha 10 com sentido a 'Gare dAusterlitz' com inicio as 5h30 e fim as 1h47 em dias de sexta e sabado.
+horario(10,'Gare dAusterlitz',5,30,1,47,1).
 
 %% ## LINHA 11 ##
-%%linha 11 com sentido a 'Chatelet' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
-horario(11,'Chatelet',5,30,0,42,0).
-%%linha 11 com sentido a 'Chatelet' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
-horario(11,'Chatelet',5,30,1,42,1).
+%%linha 11 com sentido a 'Chatelet' com inicio as 5h30 e fim as 0h59 em dias da semana e domingo.
+horario(11,'Chatelet',5,30,0,59,0).
+%%linha 11 com sentido a 'Chatelet' com inicio as 5h30 e fim as 1h59 em dias de sexta e sabado.
+horario(11,'Chatelet',5,30,1,59,1).
 
-%%linha 11 com sentido a 'Mairie des Lilas' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
-horario(11,'Mairie des Lilas',5,30,0,41,0).
-%%linha 11 com sentido a 'Mairie des Lilas' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
-horario(11,'Mairie des Lilas',5,30,1,41,1).
+%%linha 11 com sentido a 'Mairie des Lilas' com inicio as 5h30 e fim as 0h56 em dias da semana e domingo.
+horario(11,'Mairie des Lilas',5,30,0,56,0).
+%%linha 11 com sentido a 'Mairie des Lilas' com inicio as 5h30 e fim as 1h56 em dias de sexta e sabado.
+horario(11,'Mairie des Lilas',5,30,1,56,1).
 
 %% ## LINHA 12 ##
-%%linha 12 com sentido a 'Porte de la Chapelle' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
-horario(12,'Porte de la Chapelle',5,30,0,42,0).
-%%linha 12 com sentido a 'Porte de la Chapelle' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
-horario(12,'Porte de la Chapelle',5,30,1,42,1).
+%%linha 12 com sentido a 'Porte de la Chapelle' com inicio as 5h30 e fim as 0h38 em dias da semana e domingo.
+horario(12,'Porte de la Chapelle',5,30,0,38,0).
+%%linha 12 com sentido a 'Porte de la Chapelle' com inicio as 5h30 e fim as 1h38 em dias de sexta e sabado.
+horario(12,'Porte de la Chapelle',5,30,1,38,1).
 
-%%linha 12 com sentido a 'Mairie dIssy' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
-horario(12,'Mairie dIssy',5,30,0,41,0).
-%%linha 12 com sentido a 'Mairie dIssy' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
-horario(12,'Mairie dIssy',5,30,1,41,1).
+%%linha 12 com sentido a 'Mairie dIssy' com inicio as 5h30 e fim as 0h39 em dias da semana e domingo.
+horario(12,'Mairie dIssy',5,30,0,39,0).
+%%linha 12 com sentido a 'Mairie dIssy' com inicio as 5h30 e fim as 1h39 em dias de sexta e sabado.
+horario(12,'Mairie dIssy',5,30,1,39,1).
 
 %% ## LINHA 13 ##
-%%linha 13 com sentido a 'Asnieres — Gennevilliers Les Courtilles' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
-horario(13,'Asnieres — Gennevilliers Les Courtilles',5,30,0,42,0).
-%%linha 13 com sentido a 'Asnieres — Gennevilliers Les Courtilles' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
-horario(13,'Asnieres — Gennevilliers Les Courtilles',5,30,1,42,1).
+%%linha 13 com sentido a 'Asnieres — Gennevilliers Les Courtilles' com inicio as 5h30 e fim as 0h40 em dias da semana e domingo.
+horario(13,'Asnieres — Gennevilliers Les Courtilles',5,30,0,40,0).
+%%linha 13 com sentido a 'Asnieres — Gennevilliers Les Courtilles' com inicio as 5h30 e fim as 1h40 em dias de sexta e sabado.
+horario(13,'Asnieres — Gennevilliers Les Courtilles',5,30,1,40,1).
 
-%%linha 13 com sentido a 'Chatillon — Montrouge' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
-horario(13,'Chatillon — Montrouge',5,30,0,41,0).
-%%linha 13 com sentido a 'Chatillon — Montrouge' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
-horario(13,'Chatillon — Montrouge',5,30,1,41,1).
+%%linha 13 com sentido a 'Chatillon — Montrouge' com inicio as 5h30 e fim as 0h36 em dias da semana e domingo.
+horario(13,'Chatillon — Montrouge',5,30,0,36,0).
+%%linha 13 com sentido a 'Chatillon — Montrouge' com inicio as 5h30 e fim as 1h36 em dias de sexta e sabado.
+horario(13,'Chatillon — Montrouge',5,30,1,36,1).
 
 %% ## LINHA 14 ##
-%%linha 14 com sentido a 'Saint-Lazare' com inicio as 5h30 e fim as 0h42 em dias da semana e domingo.
-horario(14,'Saint-Lazare',5,30,0,42,0).
-%%linha 14 com sentido a 'Saint-Lazare' com inicio as 5h30 e fim as 1h42 em dias de sexta e sabado.
-horario(14,'Saint-Lazare',5,30,1,42,1).
+%%linha 14 com sentido a 'Saint-Lazare' com inicio as 5h30 e fim as 1h01 em dias da semana e domingo.
+horario(14,'Saint-Lazare',5,30,1,1,0).
+%%linha 14 com sentido a 'Saint-Lazare' com inicio as 5h30 e fim as 2h01 em dias de sexta e sabado.
+horario(14,'Saint-Lazare',5,30,2,1,1).
 
-%%linha 14 com sentido a 'Olympiades' com inicio as 5h30 e fim as 0h41 em dias da semana e domingo.
-horario(14,'Olympiades',5,30,0,41,0).
-%%linha 14 com sentido a 'Olympiades' com inicio as 5h30 e fim as 1h41 em dias de sexta e sabado.
-horario(14,'Olympiades',5,30,1,41,1).
+%%linha 14 com sentido a 'Olympiades' com inicio as 5h30 e fim as 1h00 em dias da semana e domingo.
+horario(14,'Olympiades',5,30,1,0,0).
+%%linha 14 com sentido a 'Olympiades' com inicio as 5h30 e fim as 2h00 em dias de sexta e sabado.
+horario(14,'Olympiades',5,30,2,0,1).
